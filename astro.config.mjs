cc8cf3479b52230ca959f787139aeb394da62409
miketpac <160641779+miketpac@users.noreply.github.com>
2024-06-14 21:35:02 -0500

    update base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://miketpac.github.io',
-  base: '/momo-landing-astro',
+  site: 'https://asdasdsdsad.github.io',
 });
