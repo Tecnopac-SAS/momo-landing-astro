431ccd302e6a5172e3c6a33003cd2d40e149c25f
miketpac <160641779+miketpac@users.noreply.github.com>
2024-06-14 23:07:14 -0500

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://momo.github.io',
-
-});
+  site: "https://miketpac.github.io/momo-landing/",
+  base: "/momo-landing-astro",
+})
