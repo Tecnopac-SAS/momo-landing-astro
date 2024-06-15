b511aedc634a333033eef52de54f00cc152f9499
miketpac <160641779+miketpac@users.noreply.github.com>
2024-06-15 16:04:06 -0500

    feat: :rocket: Deploy to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import tailwind from "@astrojs/tailwind"
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://miketpac.github.io/momo-landing/",
-
-})
+  output: "server",
+  adapter: netlify()
+});
